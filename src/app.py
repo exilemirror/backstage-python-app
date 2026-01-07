9741d58d270f3b982a8eb18783bbf9d303168fec
Hasshim Ali <hasshim.ali@thalesdigital.io>
2026-01-07 15:23:23 +0800

    Updated app with k8s and helm files

---
+++ src/app.py
@@ -0,0 +1,31 @@
+from flask import Flask, jsonify
+import datetime
+import socket
+
+app = Flask(__name__)
+
+@app.route('/')
+
+def home():
+    return '<h1>Welcome to Backstage Python App</h1>'
+
+@app.route('/api/v1/details')
+
+def details():
+    return jsonify({
+        'message': 'Welcome to the Details page.',
+        'time': datetime.datetime.now().strftime("%I:%M:%S%p on %B %d, %Y"),
+        'hostname': socket.gethostname()
+
+    })
+
+@app.route('/api/v1/healthz')
+
+def health():
+    return jsonify({
+        'status': 'up'
+    }), 200
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0")
