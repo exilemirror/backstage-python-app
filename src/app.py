96e8cb54491d58496eed761e244b11a1e964d9ad
Hasshim Ali <hasshim.ali@thalesdigital.io>
2026-01-08 10:01:05 +0800

    Updated message in app.py

--- src/app.py
+++ src/app.py
@@ -16,7 +16,7 @@ def details():
         "message": "Welcome to the Details page.",
         "time": datetime.datetime.now().strftime("%I:%M:%S%p on %B %d, %Y"),
         "hostname": socket.gethostname(),
-        "message": "Hello World!"
+        "message": "Hello and welcome to Backstage Demo!!"
 
     })
 
